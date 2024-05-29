90e286f0268a527f41bbff98d0803990653437bf
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-05-29 12:27:27 +0700

    remove debug

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "../src/assets/style.css";
 import router from "./router";
 
-console.log(process.env.VUE_APP_API_URL)
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
