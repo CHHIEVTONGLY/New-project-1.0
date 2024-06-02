33c5d92011ecf25574c53ca9844531ce81b8ec6d
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-06-02 13:38:05 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.VUE_APP_API_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
