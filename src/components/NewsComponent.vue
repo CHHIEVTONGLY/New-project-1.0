e0a12d78a9afc1e54b4cf09c91f7367b7be2cd28
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-06-03 19:48:40 +0700

    Make responsive design on md screen

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -16,51 +16,57 @@
         <h1 class="text-white text-xl font-bold">{{ headerData.title }}</h1>
       </div>
     </div>
-    <!-- local news title -->
-    <div class="flex p-4">
-      <router-link
-        to="/local/page/1"
-        class="text-larger cursor-pointer font-bold font-Helvetica"
-        >Local news</router-link
-      >
-    </div>
+
     <!-- Component -->
-    <div
-      v-for="x in newsData"
-      :key="x._id"
-      @click="getData(x._id)"
-      class="flex flex-row border-2 p-2 border-gray-100 rounded-_2lg bg-gray-100 mt-2 mb-2"
-    >
-      <div class="basis-1/3">
-        <img
-          class="w-24 h-24 ml-2 object-cover rounded-_2lg"
-          :src="x.imgUrl"
-          alt=""
-        />
-      </div>
-      <div class="flex flex-col justify-between basis-3/4 p-2">
-        <div>
-          <h1 class="font-bold font-Helvetica">
-            {{ limitLength(x.title, 50) }}
-          </h1>
-          <p>{{ limitLength(x.paragraph, 50) }}</p>
-        </div>
-        <span class="flex justify-end text-gray-400 text-smallest">
-          <span class="mt-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16m1-8h4v2h-6V7h2z"
-              />
-            </svg>
-          </span>
-          {{ x.date }}</span
+    <div>
+      <!-- local news title -->
+      <div class="flex p-4">
+        <router-link
+          to="/local/page/1"
+          class="text-larger cursor-pointer font-bold font-Helvetica"
+          >Local news</router-link
         >
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <!-- New component -->
+        <div
+          v-for="x in newsData"
+          :key="x._id"
+          @click="getData(x._id)"
+          class="flex flex-row border-2 p-2 border-gray-100 rounded-_2lg bg-gray-100 mt-2 mb-2 md:p-0 md:grid md:rounded-none md:flex-col"
+        >
+          <div class="basis-1/3">
+            <img
+              class="w-24 h-24 object-cover rounded-_2lg md:w-full md:h-48 md:rounded-none"
+              :src="x.imgUrl"
+              alt=""
+            />
+          </div>
+          <div class="flex flex-col justify-between basis-3/4 p-2">
+            <div>
+              <h1 class="font-bold font-Helvetica">
+                {{ limitLength(x.title, 50) }}
+              </h1>
+              <p>{{ limitLength(x.paragraph, 50) }}</p>
+            </div>
+            <span class="flex justify-end text-gray-400 text-smallest">
+              <span class="mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16m1-8h4v2h-6V7h2z"
+                  />
+                </svg>
+              </span>
+              {{ x.date }}</span
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- src/views/NavigationBar.vue
+++ src/views/NavigationBar.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="py-8 bg-black text-white">
     <h1
-      class="flex justify-center text-4xl sm:text-5xl lg:text-6xl font-Roboto font-bold"
+      class="flex justify-center text-3xl sm:text-5xl lg:text-6xl font-bold"
     >
       LCT NEWS
     </h1>
     <div
-      class="flex justify-center flex-wrap gap-4 sm:gap-10 text-lg sm:text-xl lg:text-2xl mt-12"
+      class="flex justify-center text-sm flex-wrap gap-4 sm:gap-10  sm:text-xl lg:text-2xl mt-12"
     >
       <router-link to="/" class="block px-3 py-2">HOME</router-link>
       <router-link to="/local/page/1" class="block px-3 py-2">LOCAL</router-link>
       <router-link to="/world/page/1" class="block px-3 py-2">WORLD</router-link>
-      <a href="#" class="block px-3 py-2">ABOUT</a>
+      <router-link to="#" class="block px-3 py-2">ABOUT</router-link>
     </div>
   </div>
 </template>
