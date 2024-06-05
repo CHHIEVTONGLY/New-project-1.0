d08c7dd517ec949844c2296a256835ed5d7418b7
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-06-05 12:28:43 +0700

    styling newsShowing

--- src/views/NewsShowing.vue
+++ src/views/NewsShowing.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="border-b-2 pb-4 md:p-24">
     <div class="p-6 md:p-12 shadow-lg rounded-lg flex flex-col md:flex-row">
-      <img :src="newsData.imgUrl" alt="" class="w-full h-72 rounded-_5lg md:rounded-lg md:w-1/3 object-cover" />
+      <img
+        :src="newsData.imgUrl"
+        alt=""
+        class="w-full h-72 rounded-_5lg md:rounded-lg md:w-1/3 object-cover"
+      />
       <div class="flex flex-col ml-0 md:ml-4 mt-4 md:mt-0">
         <h1 class="text-gray-500">{{ newsData.date }}</h1>
-        <h1 class="font-bold text-3xl md:text-5xl">{{ newsData.title }}</h1>
-        <p class="mt-2">
-          {{ newsData.paragraph }}
-        </p>
+        <h1 class="font-bold text-2xl md:text-3xl">{{ newsData.title }}</h1>
+        <p
+          class="mt-2 text-lg"
+          v-html="formatParagraph(newsData.paragraph)"
+        ></p>
         <div class="mt-auto">
           <h1 class="text-gray-500">{{ newsData.view }} Views</h1>
         </div>
@@ -19,15 +24,15 @@
       </h1>
     </div>
     <!-- Relate News component -->
-    <div class=" md:grid md:grid-cols-2 ">
+    <div class="md:grid md:grid-cols-2">
       <div
         v-for="x in popularPosts"
         @click="getData(x._id)"
         :key="x._id"
-        class=" flex flex-row px-4 py-2 border-b-2"
+        class="flex flex-row px-4 py-2 border-b-2"
       >
         <img class="w-20 h-20 object-cover rounded-lg" :src="x.imgUrl" alt="" />
-        <div class="flex flex-col mx-4 md:ml-4 ">
+        <div class="flex flex-col mx-4 md:ml-4">
           <strong>{{ x.title }}</strong>
           <p>
             {{ limitLength(x.paragraph, 40) }}
@@ -57,6 +62,8 @@ export default {
       popularPosts: [],
       pollingTimer: null,
       pollingTimer1: null,
+      originalParagraph:
+        "TikTok says it is addressing a cyber-attack that targeted several brands and celebrities.. The video-sharing app, which is owned by Chinese company ByteDance, told the BBC that a 'very limited' number of accounts had been compromised.. It added that it was working with users to restore access to their accounts. TikTok did not share further details on the perpetrators of the cyber-attack, or how it was carried out.. One of the compromised accounts belonged to news outlet CNN.. 'We have been collaborating closely with CNN to restore account access and implement enhanced security measures to safeguard their account moving forward,' a TikTok spokesperson said. 'We are dedicated to maintaining the integrity of the platform and will continue to monitor for any further inauthentic activity.' CNN did not immediately respond to a BBC request for comment.. TikTok said the account of reality TV star Paris Hilton was also targeted, but it was not compromised. Hilton, who has more than 10 million followers on TikTok, is an active user of the platform.",
     };
   },
   methods: {
@@ -118,6 +125,12 @@ export default {
       return text.length <= maxLength
         ? text
         : text.substring(0, maxLength) + "...";
+    },
+    formatParagraph(paragraph) {
+      if (!paragraph) return ""; // Return empty string if paragraph is undefined or empty
+
+      // Join the sentences back together with "<br>" inserted between them
+      return paragraph.split("|").join("<br><br>");
     },
   },
 };
